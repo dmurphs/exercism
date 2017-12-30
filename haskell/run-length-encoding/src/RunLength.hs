c9255c932775d53812c1b81521a20d5cc8a23bb2
Dan Murphy <d.p.murph19@gmail.com>
2017-12-30 13:54:06 -0700

    altered encoder to use trifecta parser functionality

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -13,8 +13,8 @@ decode encodedText = case (parser encodedText) of
 
 type IntegerOrChar = Either Integer Char
 
-decodeCharParser :: Parser String
-decodeCharParser = do
+decodeCharacterRun :: Parser String
+decodeCharacterRun = do
   firstComponent <- (Left <$> integer') <|> (Right <$> anyChar)
   case firstComponent of
     Left number -> do
@@ -24,31 +24,28 @@ decodeCharParser = do
 
 decodeParser :: Parser String
 decodeParser = do
-  expandedChars <- many decodeCharParser
+  expandedChars <- many decodeCharacterRun
   return $ concat expandedChars
 
 -- ENCODER
 
 encode :: String -> String
-encode "" = ""
-encode (x:xs) = encode' (xs) (x, 1)
+encode decodedText = case (parser decodedText) of
+  Success encoded -> encoded
+  _               -> "can't encode given text"
+  where parser = parseString encodeParser mempty
 
--- encodeCharacterRun :: Char -> Parser String
--- encodeCharacterRun c = do
---   run <- many $ char c
---   return $ case run of
---     "" -> ""
---     [c] -> [c]
---     (x:xs) -> (show $ length run) ++ [c]
+encodeCharacterRun :: Parser String
+encodeCharacterRun = do
+  c         <- anyChar
+  remaining <- many $ char c
+  let run = [c] ++ remaining
+  return $ case run of
+    "" -> ""
+    [c] -> [c]
+    (x:xs) -> (show $ length run) ++ [c]
 
-encode' :: String -> (Char,Int) -> String
-encode' "" runData = encodeRun runData
-encode' (nextChar:remaining) (currentChar,count) = if nextChar == currentChar
-  then encode' remaining (nextChar,count+1)
-  else (encodeRun (currentChar,count)) ++ (encode' remaining (nextChar,1))
-
-encodeRun :: (Char,Int) -> String
-encodeRun (c,count) = case count of
-  0 -> ""
-  1 -> [c]
-  _ -> show count ++ [c]
+encodeParser :: Parser String
+encodeParser = do
+  encoded <- many encodeCharacterRun
+  return $ concat encoded
