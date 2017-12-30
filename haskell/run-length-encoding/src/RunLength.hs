87800ce21406591560913ea878bf9e813c692ee1
Dan Murphy <d.p.murph19@gmail.com>
2017-12-30 13:21:47 -0700

    finished run length encoder/decoder

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -3,14 +3,13 @@ module RunLength (decode, encode) where
 import Control.Applicative
 import Text.Trifecta
 
+-- DECODER
+
 decode :: String -> String
 decode encodedText = case (parser encodedText) of
   Success decoded -> decoded
   _               -> "encoded text is malformed"
   where parser = parseString decodeParser mempty
-
-encode :: String -> String
-encode text = error "You need to implement this function."
 
 type IntegerOrChar = Either Integer Char
 
@@ -27,3 +26,29 @@ decodeParser :: Parser String
 decodeParser = do
   expandedChars <- many decodeCharParser
   return $ concat expandedChars
+
+-- ENCODER
+
+encode :: String -> String
+encode "" = ""
+encode (x:xs) = encode' (xs) (x, 1)
+
+-- encodeCharacterRun :: Char -> Parser String
+-- encodeCharacterRun c = do
+--   run <- many $ char c
+--   return $ case run of
+--     "" -> ""
+--     [c] -> [c]
+--     (x:xs) -> (show $ length run) ++ [c]
+
+encode' :: String -> (Char,Int) -> String
+encode' "" runData = encodeRun runData
+encode' (nextChar:remaining) (currentChar,count) = if nextChar == currentChar
+  then encode' remaining (nextChar,count+1)
+  else (encodeRun (currentChar,count)) ++ (encode' remaining (nextChar,1))
+
+encodeRun :: (Char,Int) -> String
+encodeRun (c,count) = case count of
+  0 -> ""
+  1 -> [c]
+  _ -> show count ++ [c]
