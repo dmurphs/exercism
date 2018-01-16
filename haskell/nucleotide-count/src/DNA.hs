2aab7f17eb307b5c83f06ef685d6936250bd8b6a
Dan Murphy <d.p.murph19@gmail.com>
2018-01-15 22:38:07 -0700

    Finished nucleotide haskell exercise

---
+++ haskell/nucleotide-count/src/DNA.hs
@@ -0,0 +1,24 @@
+module DNA (nucleotideCounts) where
+
+import Control.Monad
+import Data.Map (Map, fromListWith, keysSet)
+import qualified Data.Set as S (fromList)
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts xs = if S.fromList nucleotides == keysSet frequencies
+  then Right frequencies
+  else Left "Sequence contains non nucleotide character"
+  where
+    frequencies = fromListWith (+) (baseFrequencies ++ [(x,1) | x <- xs])
+    baseFrequencies = [(x,0) | x <- nucleotides]
+    nucleotides = "ACGT"
+
+-- The following is an alternate implementation, in general it performs worse,
+-- however it does do well in the case where there is 'bad' nucleotide early on
+-- as it will not continue recursion.
+
+-- nucleotideCounts :: String -> Either String (Map Char Int)
+-- nucleotideCounts []     = Right $ fromList [(x,0) | x <- nucleotides]
+-- nucleotideCounts (x:xs) = if elem x nucleotides
+--   then liftM2 (unionWith (+)) (Right (fromList [(x,1)])) (nucleotideCounts xs)
+--   else Left "Encountered non-nucleotide character"
